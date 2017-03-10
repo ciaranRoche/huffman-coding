207ea066029fbc2cc9d849588bfa185c771e93d0
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-03-10 20:16:41 +0000

    saving to external file of choice

--- src/controllers/Client.java
+++ src/controllers/Client.java
@@ -2,10 +2,13 @@ package controllers;
 
 
 import asg.cliche.Command;
+import asg.cliche.Param;
 import asg.cliche.Shell;
 import asg.cliche.ShellFactory;
 import models.Data;
 import models.Huffman;
+
+import java.util.Scanner;
 
 /**
  * Created by Ciaran Roche on 09/03/17.
@@ -14,19 +17,39 @@ public class Client {
     public Huffman huff;
     public HuffmanAPI huffAPI;
     public Data data;
+    public static Scanner scanner;
+
 
     public void Client(){}
 
     public static void main(String[] args) throws Exception{
         Client client = new Client();
+        scanner = new Scanner(System.in);
         Shell shell = ShellFactory.createConsoleShell("HUFF", "<<<<------------------------------------------------->>>>\n             Welcome to the Huffman Generator\n<<<<------------------------------------------------->>>>\n- ?help for instructions\n- ?list for commands",client);
         shell.commandLoop();
     }
 
     @Command(description = "Load External File")
-    public String load() throws Exception{
-        huffAPI.prime();
-        return "File Loaded";
+    public void load() throws Exception{
+        System.out.println("Please enter where to save the file: ");
+        String save = scanner.nextLine();
+
+        System.out.println("Please give path to file to be encoded: ");
+        String file = scanner.nextLine();
+        String data = huffAPI.prime(file);
+        huffAPI.encode(data, save);
+
+
+    }
+
+    @Command(description = "Enter String to be Encoded")
+    public void enter() throws Exception {
+        System.out.println("Please enter where to save the file: ");
+        String save = scanner.nextLine();
+
+        System.out.println("Please enter what ever you would like to see in compressed 1`s and 0`s : ");
+        String text = scanner.nextLine();
+        huffAPI.encode(text, save);
     }
 
 }
--- src/controllers/HuffmanAPI.java
+++ src/controllers/HuffmanAPI.java
@@ -7,38 +7,61 @@ import utils.FileInput;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Ciaran Roche on 09/03/17.
  */
 public class HuffmanAPI {
 
-    Huffman huff;
-    public static String dataString = "";
+    private static Huffman huff;
+    private static String dataString = "";
+    public static Scanner input;
 
     public HuffmanAPI(){
         huff = new Huffman();
+        input = new Scanner(System.in);
     }
 
-    public static void main(String[] args) throws Exception {
-        prime();
-        System.out.println(dataString);
-        write(dataString);
-    }
-
-    public static void prime() throws Exception{
+    public static String prime(String file) throws Exception{
         FileInput in = new FileInput();
-        List<Data> d = in.loadData("././data/data");
+        List<Data> d = in.loadData(file);
         for(Data data : d){
             dataString = dataString + data;
         }
+        return dataString;
     }
 
-    public static void write(String s) throws Exception{
-        final OutputStream os = new FileOutputStream("././data/result");
+    public static void encode(String s, String st) throws Exception{
+        HashMap<Character, Integer> frequencies = huff.frequencies(s);
+        Huffman.Node root = huff.huffmanTree(frequencies);
+
+        HashMap<Character, String> encoded = huff.encodied(root);
+
+        String huff = "";
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (encoded.containsKey(ch)) {
+                huff = huff + encoded.get(ch);
+            }
+        }
+
+        System.out.println("The huffman encoded string of bits for: \n" + s + "\nis: \n" + huff);
+        System.out.println("The key for the code is: \n" + encoded);
+
+        final OutputStream os = new FileOutputStream(st);
         final PrintStream ps = new PrintStream(os);
-        ps.print(s);
+        ps.print("The huffman encoded string of bits for: \n" + s + "\n\nis: \n" + huff);
         ps.close();
+        System.out.println("\nFile saved to: " + s);
     }
+
+    public String getDataString(){return dataString;}
+
+    public Huffman getHuff(){return huff;}
+
+
+
 }
